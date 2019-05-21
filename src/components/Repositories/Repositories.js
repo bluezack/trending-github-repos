f40625fd933b661b71a8209e000fc9fc6a44f2d9
zakaria <zakariaalazrake@gmail.com>
2019-05-21 00:09:35 +0000

    finished the app structure

--- src/components/Repositories/Repositories.js
+++ src/components/Repositories/Repositories.js
@@ -1,32 +1,58 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import moment from 'moment'
+
 import Repository from '../Repository/Repository'
+import './Repositories.css'
 
 export class Repositories extends Component {
     state = {
         repositories: [],
-        itemsPerPage: 30,
+        itemsPerPage: 8,
         totalPages: 0,
-        scrolling: false
+        scrolling: false,
+        errors: []
     }
+
+    date = moment().subtract(1, 'months').format('YYYY-MM-DD');
 
 
     componentDidMount() {
-        axios.get('https://api.github.com/search/repositories', { params: { q: 'created:>2017-10-22', sort: 'stars', order: 'desc' } })
+        axios.get('https://api.github.com/search/repositories', {
+            params: { q: `created:>${this.date}`, sort: 'stars', order: 'desc', per_page: this.state.itemsPerPage },
+            onDownloadProgress: (progressEvent) => {
+                let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                console.log(progressEvent.lengthComputable)
+                console.log(percentCompleted);
+            }
+        })
             .then(res => {
-                const newRepositories = res.data.items.map(r => ({ id: r.id, name: r.name, desc: r.description }))
+                const newRepositories = res.data.items.map(r => ({
+                    id: r.id,
+                    name: r.name,
+                    desc: r.description,
+                    url: r.html_url,
+                    owner: { avatar: r.owner.avatar_url, url: r.owner.html_url, name: r.owner.login },
+                    stars: r.watchers_count,
+                    issues: r.open_issues_count,
+                    date: r.created_at
+                }))
                 const { repositories } = this.state
                 this.setState({
                     repositories: [...repositories, ...newRepositories]
                 })
             })
+            .catch(err => this.state.errors.push({ error: err }))
     }
 
 
     render() {
         return (
-            <div>
-                <h3>Repositories</h3>
+            <div className="Repositories">
+                <h2>Trending Github Repositories</h2>
+                <ul>
+                    {this.state.errors.map(error => (<li>error</li>))}
+                </ul>
                 <ul>
                     {this.state.repositories.map(repo => <li key={repo.id}><Repository {...repo} /></li>)}
                 </ul>
--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import moment from 'moment'
+
+import './Repository.css'
 
 export default function Repository(props) {
-    const { id, name, desc } = props;
+    const { name, desc, owner, stars, issues, date } = props;
     return (
-        <div>
-            <h3>{name}</h3>
-            <p>{desc}</p>
+        <div className="Repository">
+            <div className="owner-image">
+                <a href={owner.url}><img src={owner.avatar} alt={owner.url} /></a>
+            </div>
+            <div className="content">
+                <h3>{name}</h3>
+                <p>{desc}</p>
+                <div className="info">
+                    <span>Stars: {stars}</span>
+                    <span>Issues: {issues}</span>
+                    <span>Submitted: <strong>{moment(date).fromNow()}</strong> By: <a href={owner.url}>{owner.name}</a></span>
+                </div>
+            </div>
         </div>
     )
 }
